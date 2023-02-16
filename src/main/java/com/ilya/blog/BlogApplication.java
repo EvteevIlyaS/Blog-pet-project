e94ea3ded638847f1656efe04e8f3cf9042f38ee
Ilya Evteev <evteev-ilya@inbox.ru>
2023-02-16 16:05:14 +0300

    first commit

---
+++ src/main/java/com/ilya/blog/BlogApplication.java
@@ -0,0 +1,15 @@
+package com.ilya.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication()
+public class BlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class, args);
+    }
+
+}
